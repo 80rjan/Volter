833f9e48e83c7faa64afcb161e2ff51a2dda4eab
Borjan Gjorgjievski <borjan@Mac.home>
2026-03-06 13:54:53 +0100

    Initialize backend java spring boot folder

---
+++ backend/src/test/java/com/volter/backend/VolterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.volter.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
